2b515551d2bab70ee6d31e2fbc4bbb85d3f253fe
Akshay Shinde <akshayshinde281195@gmail.com>
2026-01-31 17:55:28 +0530

    Spring Basic Security Code

---
+++ src/main/java/com/example/SBW8/config/SecurityConfig.java
@@ -0,0 +1,39 @@
+package com.example.SBW8.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity(debug = true)
+public class SecurityConfig {
+
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		http
+			.csrf(csrf -> csrf.disable())
+			.authorizeHttpRequests(auth -> auth
+				.requestMatchers("/home").permitAll()
+				.anyRequest().authenticated()
+			)
+			.userDetailsService(userDetailsService())
+			.httpBasic(httpBasic -> httpBasic.realmName("MyApp"));
+			
+		return http.build();
+	}
+	
+	@Bean
+	public UserDetailsService userDetailsService() {
+		 UserDetails user = User.builder()
+	                .username("Test")
+	                .password("{noop}password123")  //https://bcrypt-generator.com/
+	                .build();
+	        return new InMemoryUserDetailsManager(user);
+	}	
+}
---
+++ src/main/java/com/example/SBW8/controller/EmployeeController.java
@@ -0,0 +1,32 @@
+package com.example.SBW8.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.SBW8.entity.Employee;
+import com.example.SBW8.service.EmployeeService;
+
+@RestController
+public class EmployeeController {
+
+	@Autowired
+	private EmployeeService service;
+	
+	@GetMapping("/employees")
+	public String getEmployees() {
+		return "List of Tech Amplifiers' Employees";
+	}
+	
+	@GetMapping("/home")
+	public String home() {
+		return "Welcome to the Home Page!";
+	}
+	
+	@GetMapping("/employees/all")
+	public List<Employee> getAllEmployees() {
+		return service.getAllEmployees();
+	}
+}
